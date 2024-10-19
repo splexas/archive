bbf9d499ca03a1bef691c7128cef77ea498e4d79
splexas <official.fluk3@gmail.com>
2024-10-19 14:46:43 +0300

    fixed llist destroy func and optimized llist insertion to o(1)

--- include/list.h
+++ include/list.h
@@ -1,21 +1,25 @@
 #ifndef ARCHIVE_LIST
 #define ARCHIVE_LIST
 
+#include <stddef.h>
+
 typedef struct archive_list_node {
     struct archive_list_node *next;
     const char *file_name;
     unsigned int offset;
-    int len;
+    size_t len;
 } archive_list_node_t;
 
 typedef struct {
     archive_list_node_t *head;
-    int file_count;
+    archive_list_node_t *tail;
+    unsigned int file_count;
 } archive_list_t;
 
 archive_list_t *archive_list_init();
 int archive_list_add(archive_list_t *list, const char *file_name,
-                     unsigned int offset, int len);
+                     unsigned int offset, size_t len);
+archive_list_node_t *archive_list_get(archive_list_t *list, unsigned int index);
 void archive_list_destroy(archive_list_t *list);
 
 #endif
--- src/list.c
+++ src/list.c
@@ -9,46 +9,64 @@ archive_list_t *archive_list_init()
         return NULL;
 
     list->head = NULL;
+    list->tail = NULL;
     list->file_count = 0;
     return list;
 }
 
 int archive_list_add(archive_list_t *list, const char *file_name,
-                     unsigned int offset, int len)
+                     unsigned int offset, size_t len)
 {
     archive_list_node_t *new_node =
         (archive_list_node_t *)malloc(sizeof(archive_list_node_t));
     if (!new_node)
         return 1;
 
+    char *dup = strdup(file_name);
+    if (!dup) {
+        free(new_node);
+        return 1;
+    }
+    
     new_node->next = NULL;
-    new_node->file_name = strdup(file_name);
+    new_node->file_name = dup; 
     new_node->offset = offset;
     new_node->len = len;
 
     if (!list->head) {
         list->head = new_node;
+        list->tail = new_node;
     }
     else {
-        archive_list_node_t *node = list->head;
-        while (node->next)
-            node = node->next;
-        node->next = new_node;
+        list->tail->next = new_node;
+        list->tail = new_node;
     }
 
     list->file_count++;
     return 0;
 }
 
+archive_list_node_t *archive_list_get(archive_list_t *list, unsigned int index)
+{
+    archive_list_node_t *node = list->head;
+    unsigned int i = 0;
+
+    while (node) {
+        if (i == index)
+            return node;
+        node = node->next;
+        i++;
+    }
+
+    return NULL;
+}
+
 void archive_list_destroy(archive_list_t *list)
 {
-    if (!list->head)
-        return;
-
-    archive_list_node_t *prev = NULL;
     archive_list_node_t *node = list->head;
+    archive_list_node_t *prev = NULL;
 
-    while (node->next) {
+    while (node) {
         prev = node;
         node = node->next;
         free((void *)prev->file_name);
